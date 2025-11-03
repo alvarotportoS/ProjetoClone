1a3f2e599662a2e7c9b06a638b35343e91d6c846
joao <0001158197@senaimgaluno.com.br>
2025-11-03 10:54:47 -0300

    alteraçao

--- src/ProjetoClone/ProjetoClone.java
+++ src/ProjetoClone/ProjetoClone.java
@@ -2,6 +2,7 @@ package ProjetoClone;
 
 public class ProjetoClone {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("Modificado");
     }
 }
